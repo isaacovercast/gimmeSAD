f5bdae833da36b8841bca0342b825eaf6ba060b0
iovercast <isaac.overcast@gmail.com>
2016-06-05 15:49:27 -0400

    Added a set of simulations for clustered immigration

--- do_all_sims.py
+++ do_all_sims.py
@@ -7,22 +7,40 @@ import os
 
 if __name__ == "__main__":
     #NSIMS=10000
-    NSIMS = 1000000000
+    NSIMS = 0
     SIM_DIRECTORY="simout"
-    RECORDING_INTERVAL = NSIMS/100
+    #RECORDING_INTERVAL = NSIMS/100
     if not os.path.exists(SIM_DIRECTORY):
         os.mkdir(SIM_DIRECTORY)
 
     col_rates = [0.001, 0.01]
     k_vals = [1000, 10000, 100000]
 
-    for c in col_rates:
-        for k in k_vals:
-            sim_string = "K_"+str(k)+"-C_"+str(c)
-            cursim_dir = os.path.join(SIM_DIRECTORY, sim_string)
-            cmd = "./gimmeSAD.py -n "+ str(NSIMS)\
+    for i in range(0, 1):
+        for c in col_rates:
+            for k in k_vals:
+                sim_string = "K_"+str(k)+"-C_"+str(c)
+                cursim_dir = os.path.join(SIM_DIRECTORY, sim_string)
+                cmd = "./gimmeSAD.py -n "+ str(NSIMS)\
                         + " -c " + str(c)\
                         + " -k " + str(k)\
                         + " -o " + cursim_dir\
-                        + " -r " + str(RECORDING_INTERVAL)
-            subprocess.call(cmd, shell=True)
+                        + " &"
+                print(cmd)
+                subprocess.call(cmd, shell=True)
+
+        ## For each condition do the same condition but with 4 migrants 
+        ## at a time and 4x the size of K
+        for c in col_rates:
+            for k in k_vals:
+                sim_string = "K_"+str(k)+"-C_"+str(c)
+                cursim_dir = os.path.join(SIM_DIRECTORY, sim_string+"_x4")
+                cmd = "./gimmeSAD.py -n "+ str(NSIMS)\
+                        + " -c " + str(c)\
+                        + " -k " + str(k*4)\
+                        + " -o " + cursim_dir\
+                        + " -i 4 "\
+                        + " &"
+                print(cmd)
+                subprocess.call(cmd, shell=True)
+
