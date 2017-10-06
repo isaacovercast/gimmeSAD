803ee59e10f221d75924f9e139198c2c16717faf
Isaac Overcast <isaac.overcast@gmail.com>
2017-10-06 14:07:56 -0400

    Fix deme size test script.

--- deme_size_sims.py
+++ deme_size_sims.py
@@ -14,12 +14,12 @@ import os
 if __name__ == "__main__":
     #NSIMS=10000
     NSIMS = 0
-    SIM_DIRECTORY="simout"
+    SIM_DIRECTORY="deme_size_sims"
     #RECORDING_INTERVAL = NSIMS/100
     if not os.path.exists(SIM_DIRECTORY):
         os.mkdir(SIM_DIRECTORY)
 
-    col_rates = [0.001, 0.005, 0.01, 0.05, 0.1]
+    col_rates = [0.001, 0.005, 0.01]
     k_vals = [1000, 5000, 10000]
 
     for i in range(0, 1):
@@ -36,28 +36,28 @@ if __name__ == "__main__":
                 subprocess.call(cmd, shell=True)
 
         ## For each condition do the same condition but with 4 migrants 
-        ## at a time and 4x the size of K
+        ## at a time and 1/4 the size of K
         for c in col_rates:
             for k in k_vals:
                 sim_string = "K_"+str(k)+"-C_"+str(c)
                 cursim_dir = os.path.join(SIM_DIRECTORY, sim_string+"_x4")
                 cmd = "./gimmeSAD.py -n "+ str(NSIMS)\
                         + " -c " + str(c)\
-                        + " -k " + str(k*4)\
+                        + " -k " + str(k/4)\
                         + " -o " + cursim_dir\
                         + " -i 4 "\
                         + " &"
                 print(cmd)
                 subprocess.call(cmd, shell=True)
 
-        ## Also do clusters of 10 immigrants and 10x size of K 
+        ## Also do clusters of 10 immigrants and 1/10 size of K 
         for c in col_rates:
             for k in k_vals:
                 sim_string = "K_"+str(k)+"-C_"+str(c)
                 cursim_dir = os.path.join(SIM_DIRECTORY, sim_string+"_x10")
                 cmd = "./gimmeSAD.py -n "+ str(NSIMS)\
                         + " -c " + str(c)\
-                        + " -k " + str(k*10)\
+                        + " -k " + str(k/10)\
                         + " -o " + cursim_dir\
                         + " -i 10 "\
                         + " &"
